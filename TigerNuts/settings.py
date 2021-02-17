85ec4c5968854b353e4bbfb475b39acc236f37c7
hristo-grudev <hr.grudev@gmail.com>
2021-02-17 11:46:12 +0200

    static root

--- TigerNuts/settings.py
+++ TigerNuts/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = '/home/baticho/tigernuts/static/'
+STATIC_ROOT = '/home/baticho/baticho.pythonanywhere.com/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
