6bf8e0d96118bb7e641b811097238cb73bc6cb62
hristo-grudev <hr.grudev@gmail.com>
2021-02-17 11:39:27 +0200

    STAITC_ROOT

--- TigerNuts/settings.py
+++ TigerNuts/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/admin/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = '/tmp/static'
+STATIC_ROOT = '/home/baticho/tigernuts/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
